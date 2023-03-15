ed66e0be17af9b5239981e068e77c06be7dbc5b3
RysiuXD <dawid.zelinka@gmail.com>
2023-03-15 11:31:18 +0100

    mobilne menu implementation

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("test");
+
+const menuElement = document.getElementById("menu");
+const btnElement = document.getElementById("menuBtn");
+
+const toggleMenu =()=>{
+    menuElement.classList.toggle("showMenu");
+}
+btnElement.addEventListener("click", toggleMenu);
